b83e957118faa41512b277454b6303523f683a00
FIRST_NAME LAST_NAME <git@keithpitt.email>
2025-07-03 17:50:55 +0800

    Include stdlib here so we know it's a message coming from us

--- lib/stdlib/error/log.bash
+++ lib/stdlib/error/log.bash
@@ -1,5 +1,5 @@
 stdlib_error_log() {
   local caller="${BASH_SOURCE[1]}:${BASH_LINENO[2]}"
 
-  printf "%s: $1\n" "${caller}" "${@:2}" >&2
+  printf "bash+stdlib error: %s: $1\n" "${caller}" "${@:2}" >&2
 }
