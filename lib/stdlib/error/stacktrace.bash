c1b3b21719fd4c93a1338db0b1e1223426c29e67
FIRST_NAME LAST_NAME <git@keithpitt.email>
2025-07-07 17:39:57 +0800

    Stacktrace colors

--- lib/stdlib/error/stacktrace.bash
+++ lib/stdlib/error/stacktrace.bash
@@ -1,13 +1,14 @@
 stdlib_error_stacktrace() {
   local -i idx=1
   local -i length="${#BASH_SOURCE[@]}"
+  local -r wd="$(pwd)/"
 
   while [[ "$idx" -lt "$length" ]]; do
-    local source_file="${BASH_SOURCE[$idx]}"
+    local source_file="${BASH_SOURCE[$idx]/"$wd"/}"
     local func_name="${FUNCNAME[$idx]}"
     local line_no="${BASH_LINENO[$idx - 1]}"
 
-    printf "%s\n%s:%s\n\n" "$func_name" "$source_file" "$line_no" >&2
+    printf "\e[33m%s\e[0m \e[30m%s:%s\e[0m\n" "$func_name" "$source_file" "$line_no" >&2
 
     ((idx++))
   done
