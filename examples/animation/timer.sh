874fc5511951a05666e4e00d3dd8bf4c5fd282f9
Keith Pitt <me@keithpitt.com>
2025-07-09 00:31:50 +0800

    Fixed examples with new duration format

--- examples/animation/timer.sh
+++ examples/animation/timer.sh
@@ -7,20 +7,24 @@ stdlib_import "animation/timer"
 select example in simple loop reversed alternate speed; do
   case "$example" in
   simple)
-    stdlib_animation_timer --duration 1000
+    stdlib_animation_timer --duration 1000ms
     ;;
   loop)
-    stdlib_animation_timer --duration 1000 --loop
+    stdlib_animation_timer --duration 1000ms --loop
     ;;
   reversed)
-    stdlib_animation_timer --duration 1000 --reversed
+    stdlib_animation_timer --duration 1000ms --reversed
     ;;
   alternate)
-    stdlib_animation_timer --duration 1000 --loop --alternate
+    stdlib_animation_timer --duration 1000ms --loop --alternate
     ;;
   speed)
-    stdlib_animation_timer --duration 1000 --loop --alternate --fps 60
+    stdlib_animation_timer --duration 1000ms --loop --alternate --fps 60
     ;;
   esac
+  exit_status=$?
+  if [[ ! $exit_status -eq 0 ]]; then
+    exit $exit_status
+  fi
   break
 done
