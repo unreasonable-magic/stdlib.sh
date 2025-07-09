6e78b84943362f21981cea5d593e7a03942bcd13
Keith Pitt <me@keithpitt.com>
2025-07-09 09:24:16 +0800

    Returnvar support to basename

--- lib/stdlib/file/basename.sh
+++ lib/stdlib/file/basename.sh
@@ -1,9 +1,19 @@
 stdlib_file_basename() {
+  local returnvar
+  if [[ "$1" == "-v" ]]; then
+    returnvar="$2"
+    shift 2
+  fi
+
   local filename="${1##*/}"
 
   if [ $# -eq 2 ]; then
-    printf '%s\n' "${filename%${2#\*}}"
+    printf -v filename '%s' "${filename%${2#\*}}"
+  fi
+
+  if [[ -n "$returnvar" ]]; then
+    declare -g "$returnvar"="$filename"
   else
-    printf '%s\n' "$filename"
+    printf "%s\n" "${filename}"
   fi
 }
