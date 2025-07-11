67bb5f0bfe63d87fd8d3b4f539495b1c4a2567e5
FIRST_NAME LAST_NAME <git@keithpitt.email>
2025-07-11 21:37:51 +0800

    Experimenting with a pluck function

---
+++ lib/stdlib/json/pluck.bash
@@ -0,0 +1,16 @@
+stdlib_import "json/query/jsonpath2jq"
+stdlib_import "array"
+
+stdlib_json_pluck() {
+  # printf -v sanitized_query "$@"
+  # local query_arg=${ stdlib_json_query_jsonpath2jq "$sanitized_query"; }
+
+  local -a paths=()
+  for arg in "$@"; do
+    paths+=(".$arg // \"\"")
+  done
+
+  local query=${ stdlib_array_join --delim "," "${paths[@]}"; };
+
+  jq --compact-output --raw-output "$query"
+}
