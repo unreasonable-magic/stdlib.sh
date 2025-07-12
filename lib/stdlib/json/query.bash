1b3d71065dd8ecdcd2732322832efda202fbe159
Keith Pitt <me@keithpitt.com>
2025-07-12 10:16:47 +0800

    Added debug logging to json/query

--- lib/stdlib/json/query.bash
+++ lib/stdlib/json/query.bash
@@ -1,8 +1,11 @@
 stdlib_import "json/query/jsonpath2jq"
+stdlib_import "log"
 
 stdlib_json_query() {
   printf -v sanitized_query "$@"
   local query_arg=${ stdlib_json_query_jsonpath2jq "$sanitized_query"; }
+
+  stdlib_log_debug "jsonpath2jq ${sanitized_query@Q} => ${query_arg@Q}"
 
   jq --raw-output "$query_arg"
 }
