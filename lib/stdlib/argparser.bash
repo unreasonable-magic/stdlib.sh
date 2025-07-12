5e7ddfcdf54b163c84ee1dda789469d6d23e2dfb
Keith Pitt <me@keithpitt.com>
2025-07-12 10:17:13 +0800

    Added a new error type to argparser

--- lib/stdlib/argparser.bash
+++ lib/stdlib/argparser.bash
@@ -1,10 +1,13 @@
 stdlib_argparser() {
   case "$1" in
-    error/invalid_arg)
-      stdlib_error_log "${FUNCNAME[1]} invalid arg: $2"
-      ;;
-    *)
-      stdlib_error_fatal "stdlib_argparser: unknown option $1"
-      ;;
+  error/invalid_arg)
+    stdlib_error_log "${FUNCNAME[1]} invalid arg: $2"
+    ;;
+  error/missing_arg)
+    stdlib_error_log "${FUNCNAME[1]} missing arg: $2"
+    ;;
+  *)
+    stdlib_error_fatal "stdlib_argparser: unknown option $1"
+    ;;
   esac
 }
