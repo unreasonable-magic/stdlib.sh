de9fc188d2bce6ec260a8edeaf0d248c49dec042
FIRST_NAME LAST_NAME <git@keithpitt.email>
2025-07-14 23:42:36 +0800

    Reduced options in json/flatten and strick to dotnotation

--- lib/stdlib/json/flatten.bash
+++ lib/stdlib/json/flatten.bash
@@ -3,24 +3,24 @@ stdlib_import "json/query/jsonpath2jq"
 
 stdlib_json_flatten() {
   local query_arg="."
-  local prefix_arg=""
+  local prefix_arg="$."
 
-  while [ $# -gt 0 ]; do
-    case "$1" in
-      --prefix)
-        local prefix_arg="$2"
-        shift 2
-        ;;
-      --query)
-        query_arg=${ stdlib_json_query_jsonpath2jq "$2"; }
-        shift 2
-        ;;
-      *)
-        stdlib_argparser error/invalid_arg "$@"
-        return 1
-        ;;
-    esac
-  done
+  # while [ $# -gt 0 ]; do
+  #   case "$1" in
+  #     --prefix)
+  #       local prefix_arg="$2"
+  #       shift 2
+  #       ;;
+  #     --query)
+  #       query_arg=${ stdlib_json_query_jsonpath2jq "$2"; }
+  #       shift 2
+  #       ;;
+  #     *)
+  #       stdlib_argparser error/invalid_arg "$@"
+  #       return 1
+  #       ;;
+  #   esac
+  # done
 
   jq \
     --raw-output \
