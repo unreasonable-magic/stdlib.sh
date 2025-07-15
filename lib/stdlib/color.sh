cfddc1abb29f91339d2b31c92fb543c1714fcd4b
Keith Pitt <me@keithpitt.com>
2025-07-16 00:57:10 +0800

    shellcheck fixes for color.sh

--- lib/stdlib/color.sh
+++ lib/stdlib/color.sh
@@ -1,12 +1,13 @@
-COLOR_DIM="\e[2m"
-COLOR_FG_RED="\e[31m"
-COLOR_FG_GREEN="\e[32m"
-COLOR_FG_YELLOW="\e[33m"
-COLOR_FG_BLUE="\e[34m"
-COLOR_FG_MAGENTA="\e[34m"
-COLOR_FG_CYAN="\e[34m"
+stdlib_import "test"
 
-COLOR_RESET="\e[0m"
+export COLOR_DIM="\e[2m"
+export COLOR_FG_RED="\e[31m"
+export COLOR_FG_GREEN="\e[32m"
+export COLOR_FG_YELLOW="\e[33m"
+export COLOR_FG_BLUE="\e[34m"
+export COLOR_FG_MAGENTA="\e[34m"
+export COLOR_FG_CYAN="\e[34m"
+export COLOR_RESET="\e[0m"
 
 stdlib_color() {
   while IFS= read -r line; do
@@ -15,7 +16,7 @@ stdlib_color() {
 }
 
 stdlib_color_rainbow() {
-  if stdlib_test_is_command "lolcat"; then
+  if stdlib_test command/exists "lolcat"; then
     lolcat -f
   else
     while IFS= read -r line; do
