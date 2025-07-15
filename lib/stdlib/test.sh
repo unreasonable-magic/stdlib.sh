d99082bddfe665bc4f47a50544a34e3104989b28
Keith Pitt <me@keithpitt.com>
2025-07-16 00:59:05 +0800

    All new stdlib_test syntax

--- lib/stdlib/test.sh
+++ lib/stdlib/test.sh
@@ -1,19 +1,114 @@
-stdlib_test_exists() {
-  [ -e "$1" ]
-}
+stdlib_test() {
+  case "$1" in
+  # Files
+  file/exists)
+    [ -e "$2" ]
+    ;;
+  file/is_missing)
+    [ ! -e "$2" ]
+    ;;
+  file/is_empty)
+    [ ! -s "$2" ]
+    ;;
+  file/has_data)
+    [ -s "$2" ]
+    ;;
+  file/has_new_data)
+    [ -N "$2" ]
+    ;;
+  # file/is_block_device)
+  #   [ -b "$2" ]
+  #   ;;
+  # file/is_character_special_file)
+  #   [ -c "$2" ]
+  #   ;;
+  # file/has_set_group_id_bit)
+  #   [ -g "$2" ]
+  #   ;;
+  # file/has_sticky_bit)
+  #   [ -k "$2" ]
+  #   ;;
+  # file/has_set_user_id_bit)
+  #   [ -k "$2" ]
+  #   ;;
+  file/is_dir)
+    [ -d "$2" ]
+    ;;
+  file/is_regular)
+    [ -f "$2" ]
+    ;;
+  file/is_symbolic_link)
+    # [ -L "$2" ]
+    [ -h "$2" ]
+    ;;
+  file/is_fifo)
+    [ -p "$2" ]
+    ;;
+  file/is_socket)
+    [ -S "$2" ]
+    ;;
+  file/is_readable)
+    [ -r "$2" ]
+    ;;
+  file/is_writable)
+    [ -w "$2" ]
+    ;;
+  file/is_executable)
+    [ -x "$2" ]
+    ;;
+  file/is_tty)
+    [ -t "$2" ]
+    ;;
+  # file/is_newer_than)
+  #   [ "$2" -ef "$3" ]
+  #   ;;
+  # file/is_older_than)
+  #   [ "$2" -ot "$3" ]
+  #   ;;
+  # file/owned_by_group)
+  #   [ -G "$2" ]
+  #   ;;
+  # file/owned_by_user)
+  #   [ -O "$2" ]
+  #   ;;
 
-stdlib_test_is_dir() {
-  [ -d "$1" ]
-}
+  # Variables
 
-stdlib_test_is_file() {
-  [ -f "$1" ]
-}
+  var/is_set)
+    [ -v "$2" ]
+    ;;
+  var/is_nameref)
+    [ -R "$2" ]
+    ;;
 
-stdlib_test_strempty() {
-  [ -z "$1" ]
-}
+  # Commands
 
-stdlib_test_is_command() {
-  command -v "$1" 2>&1 >/dev/null
+  command/exists)
+    command -v "$2" &>/dev/null
+    ;;
+
+  # Strings
+
+  string/is_empty)
+    [ -z "$2" ]
+    ;;
+  string/is_present)
+    [ -n "$2" ]
+    ;;
+
+  # Shell
+
+  shellopt/is_enabled)
+    [ -o "$2" ]
+    ;;
+
+  # Error handling
+
+  '')
+    stdlib_argparser error/length_mismatch 1
+    ;;
+  *)
+    stdlib_argparser error/invalid_arg "$@"
+    ;;
+  esac
 }
