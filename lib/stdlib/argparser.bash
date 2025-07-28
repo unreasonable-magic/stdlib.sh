1283549d9d3d5517244504259bd8e4e823a59fd1
Keith Pitt <me@keithpitt.com>
2025-07-28 12:01:40 +0800

    Started building out more of the argparser

--- lib/stdlib/argparser.bash
+++ lib/stdlib/argparser.bash
@@ -14,3 +14,36 @@ stdlib_argparser() {
     ;;
   esac
 }
+
+stdlib_argparser_parse() {
+  # If we've not been given any params, and there's something on stdin we should
+  # read, let's set the params so we'll read them all in
+  if [[ $# -eq 0 && ! -t 0 ]]; then
+    set -- ""
+  fi
+
+  # Normalize the params into a newline seperated string, replacing "-" with
+  # reading a single line from stdin, and replacing "" with reading everything
+  # in from stdin. The reason that we use new lines is that it makes for easier
+  # parsing with kv and mapfile later on.
+  local input
+
+  while [[ $# -gt 0 ]]; do
+    local arg="$1"
+    shift 1
+
+    if [[ "$arg" == "-" ]]; then
+      read -r arg
+    elif [[ "$arg" == "" ]]; then
+      IFS= read -r -d $'\0' arg
+    fi
+
+    input+="$arg"
+
+    if [[ $# -gt 0 ]]; then
+      input+=$'\n'
+    fi
+  done
+
+  REPLY="$input"
+}
