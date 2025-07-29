d13e5234de3b641a028c85fd7fec2f5530027a0f
Keith Pitt <git@keithpitt.email>
2025-07-29 11:35:07 +0800

    Validate all colors (not just rgb)

--- lib/stdlib/color.bash
+++ lib/stdlib/color.bash
@@ -1,3 +1,4 @@
+stdlib_import "error"
 stdlib_import "color/rgb"
 stdlib_import "color/hex"
 stdlib_import "color/x11"
@@ -27,44 +28,62 @@ stdlib_color() {
     return 1
   fi
 
+  # Validate the color
+  if ! stdlib_color_validate_component "red" "${COLOR[1]}"; then
+    return 1
+  elif ! stdlib_color_validate_component "green" "${COLOR[2]}"; then
+    return 1
+  elif ! stdlib_color_validate_component "blue" "${COLOR[3]}"; then
+    return 1
+  fi
+
   # Format the color (and default to the same format as passed in)
   stdlib_color_format "${format_arg:-"${COLOR[0]}"}"
 }
 
 stdlib_color_parse() {
   if stdlib_color_rgb_parse "$1"; then return; fi
-  if stdlib_color_hex_parse "$1"; then return; fi
   if stdlib_color_x11_parse "$1"; then return; fi
+  if stdlib_color_hex_parse "$1"; then return; fi
   if stdlib_color_kv_parse "$1"; then return; fi
   if stdlib_color_ansi_parse "$1"; then return; fi
   if stdlib_color_name_parse "$1"; then return; fi
+
   return 1
 }
 
 stdlib_color_format() {
-  local format="$1"
-
-  case "$format" in
-    rgb)
-      stdlib_color_rgb_format
-      ;;
-    x11)
-      stdlib_color_x11_format
-      ;;
-    hex)
-      stdlib_color_hex_format
-      ;;
-    kv)
-      stdlib_color_kv_format
-      ;;
-    ansi)
-      stdlib_color_ansi_format
-      ;;
-    name)
-      stdlib_color_name_format
-      ;;
-    *)
-      stdlib_argparser error/invalid_arg "unknown format ${format}"
-      ;;
+  case "$1" in
+    rgb) stdlib_color_rgb_format ;;
+    x11) stdlib_color_x11_format ;;
+    hex) stdlib_color_hex_format ;;
+    kv) stdlib_color_kv_format ;;
+    ansi) stdlib_color_ansi_format ;;
+    name) stdlib_color_name_format ;;
+    *) stdlib_argparser error/invalid_arg "unknown format $1"; return 1 ;;
   esac
+}
+
+stdlib_color_validate_component() {
+  local component="$1"
+  local value="$2"
+
+  # No value, nothing to validate
+  if [[ "$value" == "" ]]; then
+    return 0
+  fi
+
+  # Make sure it's a number
+  if [[ ! "$value" =~ ^-?[0-9]+$ ]]; then
+    stdlib_error "$component($value) is not a number"
+    return 1
+  fi
+
+  # And that it's between 0 and 255
+  if [[ ${value} -lt 0 || ${value} -gt 255 ]]; then
+    stdlib_error "$component($value) is out of bounds (0-255)"
+    return 1
+  fi
+
+  return 0
 }
--- lib/stdlib/color/rgb.bash
+++ lib/stdlib/color/rgb.bash
@@ -1,25 +1,7 @@
-stdlib_import "string/count"
-stdlib_import "argparser"
-
 STDLIB_COLOR_RGB_REGEX="^[[:space:]]*rgb\([[:space:]]*([0-9]+)[[:space:]]*,?[[:space:]]*([0-9]+)[[:space:]]*,?[[:space:]]*([0-9]+)[[:space:]]*\)[[:space:]]*$"
 
 stdlib_color_rgb_format() {
-  # Now we've parsed the color, let's extract the rgb values
-  local -a rgb=()
-  rgb[0]="${COLOR[1]:-0}"
-  rgb[1]="${COLOR[2]:-0}"
-  rgb[2]="${COLOR[3]:-0}"
-
-  # Before we return the final version, make sure it looks like a valid
-  if ! stdlib_color_rgb_validate_channel "red" "${rgb[0]}"; then
-    return 1
-  elif ! stdlib_color_rgb_validate_channel "green" "${rgb[1]}"; then
-    return 1
-  elif ! stdlib_color_rgb_validate_channel "blue" "${rgb[2]}"; then
-    return 1
-  fi
-
-  printf "rgb(%s, %s, %s)\n" "${rgb[@]}"
+  printf "rgb(%s, %s, %s)\n" "${COLOR[1]}" "${COLOR[2]}" "${COLOR[3]}"
 }
 
 stdlib_color_rgb_parse() {
@@ -34,26 +16,4 @@ stdlib_color_rgb_parse() {
   fi
 
   return 1
-}
-
-stdlib_color_rgb_validate_channel() {
-  local channel="$1"
-  local value="$2"
-
-  if [[ "$value" == "" ]]; then
-    stdlib_error "$channel($value) is blank"
-    return 1
-  fi
-
-  if [[ ! "$value" =~ ^-?[0-9]+$ ]]; then
-    stdlib_error "$channel($value) is not a number"
-    return 1
-  fi
-
-  if [[ ${value} -lt 0 || ${value} -gt 255 ]]; then
-    stdlib_error "$channel($value) is out of bounds (0-255)"
-    return 1
-  fi
-
-  return 0
 }
