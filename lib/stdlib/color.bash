b17594891ec5ee8e63bd034b6b6091122875d75a
Keith Pitt <git@keithpitt.email>
2025-07-29 08:48:48 +0800

    Do a proper arg parse of inputs in stdlib_color before parsing

--- lib/stdlib/color.bash
+++ lib/stdlib/color.bash
@@ -1,7 +1,14 @@
 stdlib_import "color/parse"
 
 stdlib_color() {
-  stdlib_color_parse "$@"
+  local input="${| stdlib_argparser_parse "$@"; }"
+
+  if [[ "$input" == "" ]]; then
+    stdlib_argparser error/missing_arg "nothing to parse"
+    return 1
+  fi
+
+  stdlib_color_parse "$input"
 
   pp COLOR_RGB
 }
